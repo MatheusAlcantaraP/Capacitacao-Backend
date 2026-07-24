c48de4c78c4e5a792badbb5e655bdda1c0e7e7c1
Matheus Alcântara <matheus9522@Outlook.com>
2026-07-24 20:20:58 -0300

    chore: atualiza configuracoes do Firebase, dependencias, main.dart e tema global

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
-
 import 'providers/auth_provider.dart';
 import 'screens/start_screen.dart';
 import 'screens/home_screen.dart';
@@ -12,6 +13,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseMessaging.instance.requestPermission();
+  final token = await FirebaseMessaging.instance.getToken();
+  print(token);
+
   runApp(const ProviderScope(child: AsiCityApp()));
 }
 
@@ -25,16 +31,9 @@ class AsiCityApp extends ConsumerWidget {
     return MaterialApp(
       title: 'AsiCity',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 204, 223, 255),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 71, 143, 211),
-        ),
-      ),
+      theme: _buildAppTheme(),
       home: authState.when(
-        data: (user) =>
-            user != null ? const HomeScreen() : const StartScreen(),
+        data: (user) => user != null ? const HomeScreen() : const StartScreen(),
         loading: () => const Scaffold(
           body: Center(child: CircularProgressIndicator()),
         ),
@@ -44,4 +43,66 @@ class AsiCityApp extends ConsumerWidget {
       ),
     );
   }
+}
+
+ThemeData _buildAppTheme() {
+  final textTheme = GoogleFonts.inriaSansTextTheme().apply(
+    bodyColor: Color.fromARGB(255, 249, 252, 255),
+    displayColor: Color.fromARGB(255, 249, 252, 255),
+  );
+
+  return ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: Color.fromARGB(255, 61, 69, 83),
+    textTheme: textTheme,
+
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 61, 69, 83),
+      iconTheme: const IconThemeData(color: Color.fromARGB(255, 249, 252, 255)),
+      titleTextStyle: GoogleFonts.inriaSans(
+        color: Color.fromARGB(255, 249, 252, 255),
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+    cardTheme: CardThemeData(
+      color: Color.fromARGB(255, 57, 64, 77)
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 57, 64, 77),
+        foregroundColor: Color.fromARGB(255, 249, 252, 255),
+        side: const BorderSide(color: Color.fromARGB(255, 249, 252, 255), width: 2),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        textStyle: GoogleFonts.inriaSans(fontWeight: FontWeight.bold),
+      ),
+    ),
+
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      backgroundColor: Color.fromARGB(255, 71, 143, 211), // azul de destaque
+      foregroundColor: Color.fromARGB(255, 249, 252, 255), // ícone branco
+    ),
+
+    iconTheme: const IconThemeData(color: Color.fromARGB(255, 71, 143, 211)),
+
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: GoogleFonts.inriaSans(color: Color.fromARGB(255, 249, 252, 255)),
+      hintStyle: GoogleFonts.inriaSans(color: Color.fromARGB(255, 249, 252, 255)),
+      enabledBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 145, 161, 187)),
+      ),
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 71, 143, 211)),
+      ),
+      border: const OutlineInputBorder(),
+    ),
+
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color.fromARGB(255, 71, 143, 211),
+    ),
+
+    progressIndicatorTheme: const ProgressIndicatorThemeData(color:Color.fromARGB(255, 57, 64, 77)),
+  );
 }
